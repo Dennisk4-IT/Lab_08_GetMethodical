5a2e34d4da4e58bf64b461e0ae47376bb2df7d31
kenya dennis <dennisk4@mail.uc.edu>
2025-03-10 21:52:03 -0400

    Initial commit

---
+++ src/DevTest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class DevTest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String name = safeInput.getNonZeroLenString(in, "Enter your name");
+        System.out.println("You entered: " + name);
+
+        int age = safeInput.getInt(in, "Enter your age");
+        System.out.println("You entered: " + age);
+
+        double salary = safeInput.getDouble(in, "Enter your salary");
+        System.out.println("You entered: " + salary);
+
+        int favoriteNumber = safeInput.getRangedInt(in, "Enter a number between 1 and 10", 1, 10);
+        System.out.println("You entered: " + favoriteNumber);
+
+        boolean confirm = safeInput.getYNConfirm(in, "Would you like to continue?");
+        System.out.println("You have chosen: " + (confirm ? "Yes" : "No"));
+
+        String email = safeInput.getRegExString(in, "Enter a valid email", "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,}$");
+        System.out.println("You have entered a valid email: " + email);
+    }
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String userName = safeInput.getNonZeroLenString(in, "Enter your name");
+        int userAge = safeInput.getInt(in, "Enter your age");
+        double userGPA = safeInput.getDouble(in, "Enter your GPA");
+        boolean confirm = safeInput.getYNConfirm(in, "Do you want to proceed?");
+        String zipCode = safeInput.getRegExString(in, "Enter your ZIP code (5 digits)", "^\\d{5}$");
+
+        System.out.println("\nSummary:");
+        System.out.println("Name: " + userName);
+        System.out.println("Age: " + userAge);
+        System.out.println("GPA: " + userGPA);
+        System.out.println("Confirmed: " + (confirm ? "Yes" : "No"));
+        System.out.println("ZIP Code: " + zipCode);
+    }
+}
+
